1663e4490969b94384e6a34089228386bb1bb4d4
erikp <epsaav@gmail.com>
2021-09-22 04:46:24 +0000

    cambios21092hone

---
+++ Curso JS Moderno/07-Objetos/js/01-app.js
@@ -0,0 +1,13 @@
+//Creando objetos
+
+const nombre="Monitor de 20 pulgadas";
+const precio=300;
+const disponible=true;
+
+const producto = {
+    nombre: "Monitor de 20 pulgadas",
+    precio: 300,
+    disponible: false
+}
+
+console.log(producto);
